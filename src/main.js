f52d945aa701ba1f91b7b61fe6da5b2ccc6998f7
dominika <e258610@sbl.ch>
2024-01-25 15:34:36 +0100

    finished task 6

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 import { jsonLoader } from "./utils"
 
 function render(data, targetElement) {
+  let priceClass = "price"
 
   if (!data.description){
     data.description= "No description"
   } 
+  if (data.reducedPrice){
+    data.price = data.reducedPrice
+    priceClass += " green"
+  }
 
   targetElement.innerHTML += `
 <div class="article">
     <h3>${data.title}</h3>
     <div class="description">
         ${data.description}
+    </div>
+    <div class="${priceClass}">
+    ${data.price}
     </div>
 </div>
 `
@@ -19,7 +27,7 @@ function render(data, targetElement) {
 async function main() {
   const artList = document.getElementById("article-list")
 
-  const data = await jsonLoader("/level-01.json")
+  const data = await jsonLoader("/level-03.json")
 
   for (let i = 0; i < data.length; i++) {
     render(data[i], artList)
